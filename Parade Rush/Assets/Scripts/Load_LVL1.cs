6c89a6bbb4d51b3f93e2e60ddb2a6516c6d64068
kj432171 <kicker43216@hotmail.com>
2021-11-16 05:52:43 -0500

    level select screen

---
+++ Parade Rush/Assets/Scripts/Load_LVL1.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Load_LVL1 : MonoBehaviour
+{
+
+    public void SceneLoader()
+    {
+        SceneManager.LoadScene("LVL1");
+    }
+}
