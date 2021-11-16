e67e38e071ad5bd91ef48955f7d815a1bdef6cf2
NikoRios815 <58575679+NikoRios815@users.noreply.github.com>
2021-11-16 05:01:11 -0500

    animations

--- Parade Rush/Assets/Scripts/PlayerMovement.cs
+++ Parade Rush/Assets/Scripts/PlayerMovement.cs
@@ -9,6 +9,10 @@ public class PlayerMovement : MonoBehaviour
     public float speed;
     public float jumpspeed;
     private bool isgrounded;
+    public GameObject player;
+    public Camera cam;
+    private Animator anim;
+    private bool grounded;
 
     // Start is called before the first frame update
     void Start()
@@ -19,18 +23,37 @@ public class PlayerMovement : MonoBehaviour
     private void Awake()
     {
         playerbody = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        playerbody.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, playerbody.velocity.y);
+        float horizontalInput = Input.GetAxis("Horizontal");
+        playerbody.velocity = new Vector2(horizontalInput * speed, playerbody.velocity.y);
+        cam.transform.position = new Vector3(transform.localPosition.x + 3f, transform.localPosition.y + 2f, transform.localPosition.z - 10f);
+        
+        //flips the player image
+        if(horizontalInput > 0.01f)
+        {
+            transform.localScale = new Vector3(4.9994f, 4.9994f, 4.9994f);
+        }
+        else if (horizontalInput < -0.01f)
+        {
+            transform.localScale = new Vector3(-4.9994f, 4.9994f, 4.9994f);
+        }
 
+       //Checks when player leaves the groud after jump 
         if (Input.GetKey(KeyCode.Space) && isgrounded == true)
         {
-            playerbody.velocity = new Vector2(playerbody.velocity.x, jumpspeed);
+            Jump();
             isgrounded = false;
         }
+
+        //Set animation parameters
+        anim.SetBool("run", horizontalInput != 0);
+        anim.SetTrigger("jump");
+        anim.SetBool("grounded", grounded);
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -38,8 +61,14 @@ public class PlayerMovement : MonoBehaviour
         if(collision.gameObject.tag == "Floor")
         {
             isgrounded = true;
+            grounded = true;
         }
     }
 
+    private void Jump()
+    {
+        playerbody.velocity = new Vector2(playerbody.velocity.x, jumpspeed);
+        grounded = false;
+    }
 
 }
